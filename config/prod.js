46d5ce7ed94ade73050a96fe9bcc3155ec338272
Erce Kal <erce.kal@plentific.com>
2018-02-11 22:26:26 +0000

    env variables

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientId: process.env.googleClientId,
+  googleClientSecret: process.env.googleClientSecret,
+  mongoURIDev: process.env.mongoURIDev,
+  cookieKey: process.env.cookieKey,
+}
